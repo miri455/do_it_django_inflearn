bffbdf0c7999f83a5c7c082077ababf3345045a0
miri455 <wnslaqkrdptj@gmail.com>
2025-06-13 11:57:39 +0900

    make model and set time

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author: next time...
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'  # change to admin title like this.
 # Create your models here.

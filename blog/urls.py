2ef97a5ebcf3e8299bf5be64fa840b66ebd8c88c
miri455 <wnslaqkrdptj@gmail.com>
2025-06-29 11:02:24 +0900

    add post edit function

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # path('', views.index),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('create_post/', views.PostCreate.as_view()),
+    path('update_post/<int:pk>', views.PostUpdate.as_view()),
     path('', views.PostList.as_view()),
     path('category/<str:slug>/', views.category_page),
     path('tag/<str:slug>/', views.tag_page),

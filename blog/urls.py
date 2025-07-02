6c88f4bd9d82fc5a2d32b253bdf79978209edce2
miri455 <wnslaqkrdptj@gmail.com>
2025-07-02 09:21:12 +0900

    add comment delete function

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('create_post/', views.PostCreate.as_view()),
     path('update_post/<int:pk>', views.PostUpdate.as_view()),
     path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
+    path('delete_comment/<int:pk>/', views.delete_comment),
     path('', views.PostList.as_view()),
     path('category/<str:slug>/', views.category_page),
     path('tag/<str:slug>/', views.tag_page),

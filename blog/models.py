58a94236c99b990319838b3cc818e7a7f2dcc5a7
miri455 <wnslaqkrdptj@gmail.com>
2025-07-03 09:11:26 +0900

    reserve main page

--- blog/models.py
+++ blog/models.py
@@ -58,6 +58,12 @@ class Post(models.Model):
     def get_context_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/3051/005a59fc610ed4e4/svg/{self.author.email}'
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
